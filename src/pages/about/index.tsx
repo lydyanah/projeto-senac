72bfc12a04be28f601bfdc0821058a5002f6cdaf
Lidi <lidiana.tdsc@gmail.com>
2021-08-05 21:05:17 -0300

    página sobre

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,9 +2,25 @@ import ExternalPage from "../../components/ExternalPage/ExternalPage";
 
 const About = () => {
   return (
-      <ExternalPage>
-      <h1>Sobre nós</h1>
-      </ExternalPage>
+    <ExternalPage>
+      <div>
+        <h1>Sobre</h1>
+        <p>
+          A proposta do Meu Estilo Online é ser uma alternativa aos aplicativos
+          de virtualização de guarda-roupas já existentes, oferecendo uma
+          plataforma totalmente em português, fácil de utilizar e
+          personalizável.
+        </p>
+      </div>
+
+      <div>
+        <h2>Desenvolvedores</h2>
+        <h3>Lidiana Tedeschi</h3>
+        <p>gitHub</p>
+        <h3>Arthur Mendes</h3>
+        <p>gitHub</p>
+      </div>
+    </ExternalPage>
   );
 };
 
