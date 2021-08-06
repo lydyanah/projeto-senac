95f61b50ea8cdda1e2e8f343282e38728908e08a
Lidi <lidiana.tdsc@gmail.com>
2021-08-05 22:06:25 -0300

    página home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -24,6 +24,17 @@ const Home = () => {
         <h2>Fashionistas e Profissionais da moda</h2>
         <p>Crie novos looks, estilos e roupas, melhore o seu estilo</p>
       </div>
+      <div>
+        <h1>Recursos</h1>
+        <h2>Closet</h2>
+        <p>Cadastre suas roupas, sapatos e acessórios e veja tudo o que você tem facilmente</p>
+        <h2>Looks</h2>
+        <p>Crie e guarde combinações</p>
+        <h2>Calendário</h2>
+        <p>Planeje o que vestir antes de sair da cama</p>
+        <h2>Malas</h2>
+        <p>Planeje o que levar em suas viagens</p>
+      </div>
     </ExternalPage>
   );
 };
