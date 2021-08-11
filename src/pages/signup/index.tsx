1a0687afdd29e82cf7d710c655a10553de6d3630
arthurmendes <84751128+mendesarthur@users.noreply.github.com>
2021-08-10 21:20:20 -0300

    + conteúdo tela de login

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -3,7 +3,33 @@ import ExternalPage from "../../components/ExternalPage/ExternalPage";
 const Signup = () => {
   return (
     <ExternalPage>
-          
+          <h1>Crie uma Conta</h1> 
+        <form action="">
+        <div>
+        <label htmlFor="nome">Nome</label>
+          <input 
+          type="nome" 
+          id="nome" 
+          name="nome">
+          </input>  
+        </div>
+        </form>
+        <div>
+        <label htmlFor="email">E-mail</label>
+          <input 
+          type="email" 
+          id="email" 
+          name="email">
+          </input>  
+        </div>
+        <div>
+        <label htmlFor="password">Senha</label>
+          <input 
+          type="password" 
+          id="password" 
+          name="password">
+          </input>  
+        </div>
     </ExternalPage>
 );
 };
