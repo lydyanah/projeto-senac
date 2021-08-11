2e763d99159329d843476dd81ec6601cfc29c7ad
Lidi <lidiana.tdsc@gmail.com>
2021-08-11 12:13:45 -0300

    importação das fontes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,7 +10,7 @@ const Home = () => {
         <h1>Organize, crie, planeje!</h1>
         <p>
           Adicione suas roupas e crie looks, planeje o que vestir e tenha acesso
-          ao seu guarda-roupa em qualquer lugar que você estiver!{" "}
+          ao seu guarda-roupa em qualquer lugar que você estiver!
         </p>
         <Link to="/cadastro">
           <button>Cadastre-se</button>
@@ -18,23 +18,23 @@ const Home = () => {
       </div>
 
       <div className="públicoAlvo">
-        <h1>A plataforma é perfeita para:</h1>
-        <h2>Pessoas Ocupadas</h2>
+        <h2>A plataforma é perfeita para:</h2>
+        <h3>Pessoas Ocupadas</h3>
         <p>Planeje o que vestir rapidamente e em qualquer lugar</p>
-        <h2>Viajantes</h2>
+        <h3>Viajantes</h3>
         <p>Veja o que levar rapidamente e organize suas viagens</p>
-        <h2>Fashionistas e Profissionais da moda</h2>
+        <h3>Fashionistas e Profissionais da moda</h3>
         <p>Crie novos looks, estilos e roupas, melhore o seu estilo</p>
       </div>
       <div className="recursos">
-        <h1>Recursos</h1>
-        <h2>Closet</h2>
+        <h2>Recursos</h2>
+        <h3>Closet</h3>
         <p>Cadastre suas roupas, sapatos e acessórios e veja tudo o que você tem facilmente</p>
-        <h2>Looks</h2>
+        <h3>Looks</h3>
         <p>Crie e guarde combinações</p>
-        <h2>Calendário</h2>
+        <h3>Calendário</h3>
         <p>Planeje o que vestir antes de sair da cama</p>
-        <h2>Malas</h2>
+        <h3>Malas</h3>
         <p>Planeje o que levar em suas viagens</p>
       </div>
     </Page>
