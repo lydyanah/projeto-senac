9ea193657660710ee07f9aa3e1dec7c47d3dc185
Lidi <lidiana.tdsc@gmail.com>
2021-08-11 18:49:03 -0300

    botão github

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,4 +1,6 @@
 import Page from "../../components/Page/Page";
+import GitHubButton from "react-github-btn";
+import lydyanah from "../../assets/images/lydyanah.jpg";
 import "./styles.css";
 
 const About = () => {
@@ -20,7 +22,16 @@ const About = () => {
           <ul className="devs">
             <li>
               <h3>Lidiana Tedeschi</h3>
-              <p>gitHub</p>
+              <img className="lydyanah" src={lydyanah} alt="lydyanah" />
+              <span className="GitHubButton">
+                <GitHubButton
+                  href="https://github.com/lydyanah"
+                  data-color-scheme="no-preference: dark; light: dark; dark: dark;"
+                  data-size="large"
+                  aria-label="Siga @lydyanah no GitHub"
+                >Seguir @lydyanah
+                </GitHubButton>
+              </span>
             </li>
             <li>
               <h3>Arthur Mendes</h3>
