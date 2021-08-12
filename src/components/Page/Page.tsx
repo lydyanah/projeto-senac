8f582a376e304aa71aac288da5f14ca8f3b20059
Lidi <lidiana.tdsc@gmail.com>
2021-08-12 18:33:49 -0300

    revisão css

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -10,33 +10,33 @@ type Props = {
 const Page: FunctionComponent<Props> = ({ children }) => {
   return (
     <>
-      <div className="content">
-        <div className="navbar">
+      <div className="navbar-fixed">
+        <div className="navbar-content">
           <Link to="/">
-            <img className="logo" src={Logo} alt="logotipo" />
+            <img className="logotipo" src={Logo} alt="logotipo" />
           </Link>
-          <nav className="menu">
-            <Link to="/" className="menuItem">
-              Home
-            </Link>
-            <Link to="/sobre" className="menuItem">
-              Sobre
-            </Link>
-            <Link to="/contato" className="menuItem">
-              Contato
-            </Link>
-          </nav>
-          <Link to="/login">
-            <button className="login">Entrar</button>
-          </Link>
+          <div className="menu">
+            <nav className="menu">
+              <Link to="/" className="menuItem">
+                Home
+              </Link>
+              <Link to="/sobre" className="menuItem">
+                Sobre
+              </Link>
+              <Link to="/contato" className="menuItem">
+                Contato
+              </Link>
+              <Link to="/login">
+                <button className="login">Entrar</button>
+              </Link>
+            </nav>
+          </div>
         </div>
-        <main>
-          {children}
-        </main>
-        <footer>
-          <p>2021. Meu Estilo Online. Todos os direitos reservados.</p>
-        </footer>
       </div>
+      <main>{children}</main>
+      <footer>
+        <p>2021. Meu Estilo Online. Todos os direitos reservados.</p>
+      </footer>
     </>
   );
 };
